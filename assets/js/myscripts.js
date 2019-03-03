45c033197add4b459167c975897b7f32bfa5ed01
The Lizard <suzpaylor@gmail.com>
2019-03-03 11:14:07 -0700

    working on adding weather API

--- assets/js/myscripts.js
+++ assets/js/myscripts.js
@@ -88,3 +88,29 @@ $('#please_show2').mouseover(function(){
 });
 
 
+// //working on weather API
+// $.ajax({
+// 	url: queryURL,
+// 	method: "GET"
+// })
+// 	// We store all of the retrieved data inside of an object called "response"
+// 	.then(function(response) {
+
+// 		// Log the queryURL
+// 		console.log(queryURL);
+
+// 		// Log the resulting object
+// 		console.log(response);
+
+// 		// Transfer content to HTML
+// 		$(".city").html("<h1>" + response.name + " Weather Details</h1>");
+// 		$(".wind").text("Wind Speed: " + response.wind.speed);
+// 		$(".humidity").text("Humidity: " + response.main.humidity);
+// 		$(".temp").text("Temperature (F) " + response.main.temp);
+
+// 		// Log the data in the console as well
+// 		console.log("Wind Speed: " + response.wind.speed);
+// 		console.log("Humidity: " + response.main.humidity);
+// 		console.log("Temperature (F): " + response.main.temp);
+// 	});
+
